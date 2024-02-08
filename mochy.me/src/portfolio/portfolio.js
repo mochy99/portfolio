73af3b2a697b9a3faed1dd4d78851e271a0c1355
mochy99 <mochynguyen99@gmail.com>
2024-02-07 23:18:31 -0800

    added DataProvider component

--- mochy.me/src/portfolio/portfolio.js
+++ mochy.me/src/portfolio/portfolio.js
@@ -7,19 +7,19 @@ import Skills from "./components/skills/skills";
 import Certifications from "./components/certifications/certifications";
 import Projects from "./components/projects/projects";
 import Contact from "./components/contact/contact";
-
+import { DataProvider } from "./components/data";
 
 function Portfolio() {
     
     return (
-        <>
+        <DataProvider>
             <LandingPage />
             {/* <AboutMe />
             <Skills />
             <Certifications />
             <Projects />
             <Contact /> */}
-        </>
+        </DataProvider>
     )   
 }
 

d900655037bf42ae8639db83d64cebdb19b6c38a
mochy99 <mochynguyen99@gmail.com>
2024-02-09 23:28:09 -0800

    added icon in nav

--- mochy.me/src/portfolio/components/landingPage/nav.js
+++ mochy.me/src/portfolio/components/landingPage/nav.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DataConsumption } from "../data";
+import AboutMeIcon from "../../../img/aboutme.js";
+import CerfIcon from "../../../img/certification.js";
+import ContactIcon from "../../../img/contact.js"; 
+import HomeIcon from "../../../img/home.js";
+import ProjectIcon from "../../../img/project.js";
+import SkillIcon from "../../../img/skill.js";
+
 
 function Nav() {
-    const data = DataConsumption()
-    const nav = data.landingPage.nav
+    const data = DataConsumption();
+    const nav = data.landingPage.nav;
+    const [idNav, setIdNav] = useState('navRoll');
+    const navIcon = [HomeIcon, AboutMeIcon, SkillIcon, CerfIcon, ProjectIcon, ContactIcon];
+
     return (
-        <ul id='nav'>
-            {
-            nav.map((section, index) => (
-                <li key={index} className='submerge'>{section}</li>
-            ))
-            }
+        <ul id={idNav}>
+          {navIcon.map((Icon, i) => ( 
+            <li key={i} className={`submerge${idNav === 'nav' && i === 0 ? ' disable' : ''}`}>
+              {idNav !== 'nav' && <Icon />} 
+              <span className='iconHover'>{nav[i]}</span>
+               
+            </li>
+          ))}
         </ul>
-    )
+      );
+      
+    
 }
 
 export default Nav;

296b6165893724246bea7ee21c037f4535eacfb6
mochy99 <mochynguyen99@gmail.com>
2024-02-26 21:37:31 -0800

    basic version

--- mochy.me/src/portfolio/components/aboutMe/aboutMe.js
+++ mochy.me/src/portfolio/components/aboutMe/aboutMe.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { DataConsumption } from "../data";
 function AboutMe(props) {
-    
+    const dataAboutMe = DataConsumption().aboutMe;
+    const name = dataAboutMe.name;
+    const bio = dataAboutMe.bio;
+    const aspiration = dataAboutMe.aspiration;
+    const hobbies = dataAboutMe.hobbies;
+
+
+
+    return (
+        <div className="container">
+            <section id='aboutme' class='floaty'>
+                <h1>{name}</h1>
+                <p id='bio'>{bio}</p>
+                <p id='asp'>{aspiration}</p>
+            </section>
+        </div>
+        
+    )
 
 }
 

68baf7d294937c936303969979139667729f2373
mochy99 <mochynguyen99@gmail.com>
2024-02-28 22:21:23 -0800

    initial constants

--- mochy.me/src/portfolio/components/skills/skills.js
+++ mochy.me/src/portfolio/components/skills/skills.js
@@ -1,7 +1,10 @@
 import React from "react";
-
+import { DataConsumption } from "../data";
 function Skills(props) {
-    
+    const data = DataConsumption();
+    const programming = data.skills.programming.array;
+    const tools = data.skills.tools.array;
+
 
 }
 

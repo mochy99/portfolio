09e8dfaf2bca28b67eeabbef4b8890aae0df9ee2
mochy99 <mochynguyen99@gmail.com>
2024-02-29 22:59:10 -0800

    changed return

--- mochy.me/src/portfolio/components/skills/skills.js
+++ mochy.me/src/portfolio/components/skills/skills.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { DataConsumption } from "../data";
+import Skill from "./skill";
 function Skills(props) {
     const data = DataConsumption();
     const programming = data.skills.programming.array;
     const tools = data.skills.tools.array;
-
-
+    return (
+        <>
+            {programming.map((skill) => (
+                <Skill value={skill} />
+            ))}
+    
+            {tools.map((tool) => (
+                <Skill  />
+            ))}
+        </>
+        
+        
+    )
+    
 }
 
 export default Skills
